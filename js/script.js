8c1aabcf8aec71b418bc75f6182cb303bc96505f
alinatishen@gmail.com <vekmnzirf123>
2016-10-06 23:21:35 +0300

    add some animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 ;(function(){
-	
+
+	var d = document,
+		accLinks = d.getElementsByClassName("acc_link");
+
+	var accCallback = function() {
+	    var liClassName = this.parentNode.className;
+	    if(liClassName.indexOf("current") > -1){
+	    	this.parentNode.className = liClassName.replace(" current","");
+	    } else {
+	    	Array.from(accLinks).forEach(function(element) {
+				element.parentNode.className = element.parentNode.className.replace(" current","");
+		    });
+		    this.parentNode.className += " current";
+	    }
+	};
+
+	Array.from(accLinks).forEach(function(element) {
+		element.addEventListener('click', accCallback);
+    });
+
 }());
