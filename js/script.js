88982a4f7ac6c23e87eb2f22496cee19a6d7dbb1
alinatishen@gmail.com <vekmnzirf123>
2016-10-08 14:36:18 +0300

    fix class name and var name

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 ;(function(){
 
-	var d = document,
-		accLinks = d.getElementsByClassName("acc_link");
+	var doc = document,
+		accordionLinks = doc.getElementsByClassName("accordion_link");
 
 	setTimeout(function() {
-		d.getElementsByTagName("body")[0].className += "show";
+		doc.getElementsByTagName("body")[0].className += "show";
 	}, 0);
 	
-	var accCallback = function() {
+	var accordionCallback = function() {
 	    var liClassName = this.parentNode.className;
 	    if(liClassName.indexOf("current") > -1){
 	    	this.parentNode.className = liClassName.replace(" current","");
 	    } else {
-	    	for (var i = 0; i < accLinks.length ; i++) {
-				accLinks[i].parentNode.className = accLinks[i].parentNode.className.replace(" current","");
+	    	for (var i = 0; i < accordionLinks.length ; i++) {
+				accordionLinks[i].parentNode.className = accordionLinks[i].parentNode.className.replace(" current","");
 		    }
 		    this.parentNode.className += " current";
 	    }
 	};
 
-	for (var i = 0; i < accLinks.length ; i++) {
+	for (var i = 0; i < accordionLinks.length ; i++) {
 		if(typeof addEventListener !== 'undefined'){
-			accLinks[i].addEventListener('click', accCallback);
+			accordionLinks[i].addEventListener('click', accordionCallback);
 		} else if (typeof attachEvent !== 'undefined'){
-			accLinks[i].addEventListener('onclick', accCallback);
+			accordionLinks[i].addEventListener('onclick', accordionCallback);
 		} else {
-			accLinks[i]['onclick'] = accCallback;
+			accordionLinks[i]['onclick'] = accordionCallback;
 		}
 	}
 
